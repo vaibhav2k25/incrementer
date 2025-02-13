ac9cf23c01115ba358f7d976493414487b8f5efd
Vaibhav Srivastava <p7167472@ness.com>
2025-02-13 14:35:18 +0530

    first commit

---
+++ src/components/hooks/Incrementer.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export const Incrementer=()=>{
+    let [count,setCount] = useState(0);
+    const handleIncrement = ()=>{
+        setCount(()=>count+1);
+    }
+    return(
+        <>
+            <h1>{count}</h1>
+            <button onClick={handleIncrement}>Increment</button>
+        </>
+    );
+};
